9e24205d9b4c57ff2304542103bd50dc38ec9b3f
xTirian <tirianx@yahoo.com>
2024-04-15 20:04:20 -0300

    feat: project card (MOBILE)

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { IProjectProps } from '../../../assets/libs/models/IProject.model';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
@@ -17,5 +18,7 @@ export class CardComponent {
     link: '',
     github: '',
   };
+
+  ViewProjectLabel = 'View Project';
 }
 
