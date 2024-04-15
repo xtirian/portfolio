6f457e69a920701b16fbd8eeb199708b2f622a59
xTirian <tirianx@yahoo.com>
2024-04-15 20:03:13 -0300

    feat: add cardt to project view passing through module

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
 import { ButtonComponent } from '../../components/button/button.component';
+import { IProjectProps } from '../../../assets/libs/models/IProject.model';
 
 @Component({
   selector: 'app-projects',
@@ -14,4 +15,16 @@ export class ProjectsComponent {
     buttonLink: null,
     buttonText: 'Contact Me'
   };
+  // IMPORT FROM API
+  projects: IProjectProps[] =[
+    {title: 'BMI CALCULATOR',
+      description: 'BMI Calculator is a web application which allows the user to calculate his Body Mass Index. The user can input his weight and height in metric or imperial units and the application will calculate the BMI and display the result. The user can also see the BMI category and a description of the category. The application is responsive and can be used on mobile devices. The application is built using HTML, CSS, and JavaScript.',
+      tags: ['JAVASCRIPT','HTML','CSS'],
+      image: 'assets/images/projects/project1.webp' ,
+      link: 'https://bmi-calculator-blond-two.vercel.app/' ,
+      github: 'https://github.com/xtirian/Frontend-Mentor-SnyderCut/tree/main/bmi-calculator' ,
+    },
+
+  ]
+
 }
