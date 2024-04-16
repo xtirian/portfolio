60a911ceee0fa17fc972118260e401bd43c12efd
xTirian <tirianx@yahoo.com>
2024-04-16 01:24:45 -0300

    refact: add footer in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
 import { StacksComponent } from './views/stacks/stacks.component';
 import { ProjectsComponent } from './views/projects/projects.component';
+import { FooterComponent } from './views/footer/footer.component';
 
-const sharedComponents = [HeaderComponent, HeroBannerComponent, StacksComponent, ProjectsComponent];
+const sharedComponents = [HeaderComponent, HeroBannerComponent, StacksComponent, ProjectsComponent, FooterComponent];
 
 @Component({
   selector: 'app-root',
