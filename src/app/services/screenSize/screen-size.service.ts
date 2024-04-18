18e0a9cc43305fd2d5802bd4378b947363a779ca
xTirian <tirianx@yahoo.com>
2024-04-17 21:58:36 -0300

    feat: improving the sreen observer service

--- src/app/services/screenSize/screen-size.service.ts
+++ src/app/services/screenSize/screen-size.service.ts
@@ -2,49 +2,45 @@ import { Injectable } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScreenSizeService {
   isMobile = false;
   isTablet = false;
   isDesktop = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.breakpointObserver
-      .observe([Breakpoints.Handset, Breakpoints.Tablet, Breakpoints.Web])
-      .subscribe((result) => {
-        if (result.matches) {
-          if (
-            result.breakpoints[
-              '(max-width: 599.98px) and (orientation: portrait)'
-            ]
-          ) {
-            this.isMobile = true;
-            this.isTablet = false;
-            this.isDesktop = false;
-          }
-          if (
-            result.breakpoints[
-              '(max-width: 959.98px) and (orientation: landscape)'
-            ] ||
-            result.breakpoints[
-              '(min-width: 600px) and (max-width: 839.98px) and (orientation: portrait)'
-            ]
-          ) {
-            this.isMobile = false;
-            this.isTablet = true;
-            this.isDesktop = false;
-          }
-          if (
-            result.breakpoints[
-              '(min-width: 1280px) and (orientation: landscape)'
-            ]
-          ) {
-            this.isMobile = false;
-            this.isTablet = false;
-            this.isDesktop = true;
-          }
-        }
-      })
-   }
+  constructor() {
+    this.checkViewportSize();
+    window.addEventListener('resize', this.checkViewportSize);
+  }
+
+  checkViewportSize = () => {
+    const width = window.innerWidth;
+    if (
+      width <= 599.98 &&
+      window.matchMedia('(orientation: portrait)').matches
+    ) {
+      this.isMobile = true;
+      this.isTablet = false;
+      this.isDesktop = false;    } else if (
+      (width <= 959.98 &&
+        window.matchMedia('(orientation: landscape)').matches) ||
+      (width >= 600 &&
+        width <= 839.98 &&
+        window.matchMedia('(orientation: portrait)').matches) ||
+      (width >= 1280 && width <= 1400 && window.matchMedia('(orientation: landscape)').matches)
+    ) {
+      this.isMobile = false;
+
+      this.isTablet = true;
+
+      this.isDesktop = false;
+    } else {
+      this.isMobile = false;
+
+      this.isTablet = false;
+
+      this.isDesktop = true;
+    }
+  };
 }
